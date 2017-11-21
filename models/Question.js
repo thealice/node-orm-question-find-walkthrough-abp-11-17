e8c42003d3752fadc9fc68fb71914ed6a52be2ad
Alice Freda <alicefreda@gmail.com>
2017-11-21 02:08:22 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -7,10 +7,10 @@ class Question{
         id INTEGER PRIMARY KEY,
         content TEXT
       )`
-      
+
       db.run(sql, function(){
         resolve("questions table created")
-      })      
+      })
     })
   }
 
@@ -19,14 +19,18 @@ class Question{
   }
 
   insert(){
-    const self = this 
+    const self = this
     const sql = `INSERT INTO questions (content) VALUES (?)`
     return new Promise(function(resolve){
       db.run(sql, [self.content], function(err, result){
         self.id = this.lastID
-        resolve(self)      
+        resolve(self)
       })
     })
+  }
+
+  static Find(id) {
+    
   }
 
 }
