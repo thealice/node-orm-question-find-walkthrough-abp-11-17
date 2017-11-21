f82ad28f3ea9888e1f3b04c3c6c0c65e4612984d
Alice Freda <alicefreda@gmail.com>
2017-11-21 02:38:25 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -7,28 +7,40 @@ class Question{
         id INTEGER PRIMARY KEY,
         content TEXT
       )`
-      
+
       db.run(sql, function(){
-        resolve("questions table created")
-      })      
-    })
-  }
-
-  constructor(content){
-    this.content = content
-  }
-
-  insert(){
-    const self = this 
-    const sql = `INSERT INTO questions (content) VALUES (?)`
-    return new Promise(function(resolve){
-      db.run(sql, [self.content], function(err, result){
-        self.id = this.lastID
-        resolve(self)      
+        resolve("questions table created");
       })
     })
   }
 
+  constructor(content){
+    this.content = content;
+  }
+
+  insert(){
+    const self = this;
+    const sql = `INSERT INTO questions (content) VALUES (?)`;
+    return new Promise(function(resolve){
+      db.run(sql, [self.content], function(err, result){
+        self.id = this.lastID;
+        resolve(self);
+      })
+    })
+  }
+
+  static Find(id) {
+    const sql = `SELECT * FROM questions WHERE id = (?)`;
+    return new Promise(function(resolve){
+      db.get(sql, [id], function(err, result){
+        const question = new Question(result.content);
+        question.id = result.id;
+        resolve(question);
+
+      })
+    })
+
+  }
 }
 
 module.exports = Question;
