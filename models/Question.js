32ff0ec1c4897b4e739cc525b5ccf956da59468d
Alice Freda <alicefreda@gmail.com>
2017-11-21 02:31:29 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -34,6 +34,7 @@ class Question{
     return new Promise(function(resolve){
       db.get(sql, [id], function(err, result){
         const question = new Question();
+        question.result = result.content;
         question.id = result.id;
         resolve(question);
 
